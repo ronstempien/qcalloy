c6d2a9134902a44f8c5acfd7855d20dcd58a3d46
Ron <flavivs.clavdivs.jvlianvs@gmail.com>
2024-09-17 11:10:02 -0400

    some java work

---
+++ base.als
@@ -0,0 +1,7 @@
+module teleport
+
+open util/ordering[circGraph] as grph
+open util/integer
+
+
+
--- qcAlloy.java
+++ qcAlloy.java
@@ -7,119 +7,143 @@ import edu.mit.csail.sdg.parser.CompModule;
 import edu.mit.csail.sdg.parser.CompUtil;
 import edu.mit.csail.sdg.ast.Sig.PrimSig;
 import edu.mit.csail.sdg.ast.Sig.Field;
+import edu.mit.csail.sdg.ast.ExprConstant;
 
 import static edu.mit.csail.sdg.ast.ExprConstant.Op.*;
 
 public class qcAlloy {
-    public static void main(String[] args) {
-        try {
-            // Initialize the module
-            CompModule world = CompUtil.parseEverything_fromString(null, null, "");//new CompModule();
+	public static final int NQUBIT = 5;
+	public static final int NMACHINE = 3;
 
-            /*
-            // Create abstract sig Qubit
-            PrimSig Qubit = new PrimSig("Qubit", Attr.ABSTRACT);
+	public static final int CAPACITY = 2;
 
-            // Create concrete Qubit sigs
-            PrimSig q_0 = new PrimSig("q_0", Qubit, Attr.ONE);
-            PrimSig q_1 = new PrimSig("q_1", Qubit, Attr.ONE);
-            PrimSig q_2 = new PrimSig("q_2", Qubit, Attr.ONE);
-            PrimSig q_3 = new PrimSig("q_3", Qubit, Attr.ONE);
-            PrimSig q_4 = new PrimSig("q_4", Qubit, Attr.ONE);
+	public static void main(String[] args) {
+		// Initialize the module
+		// CompModule world = CompUtil.parseEverything_fromString(null, "");
+		
+		// Create abstract sig Qubit
+		PrimSig Qubit = new PrimSig("Qubit", Attr.ABSTRACT);
+	
+		PrimSig[] qubits = new PrimSig[NQUBIT];
+		PrimSig[] machines = new PrimSig[NMACHINE];
 
-            // Create abstract sig Machine
-            PrimSig Machine = new PrimSig("Machine", Attr.ABSTRACT);
+		// Create concrete Qubit sigs
+		for (int i = 0; i < NQUBIT; i++)
+			qubits[i] = new PrimSig("q_" + i, Qubit, Attr.ONE);
+		
+		// Create abstract sig Machine
+		PrimSig Machine = new PrimSig("Machine", Attr.ABSTRACT);
+		
+		// Create concrete Machine sigs
+		for (int i = 0; i < NMACHINE; i++)
+			machines[i] = new PrimSig("M_" + i, Machine, Attr.ONE);
+		
+		// Create the circGraph sig
+		PrimSig circGraph = new PrimSig("circGraph");
+		
+		// Fields for circGraph
+		Field edges = circGraph.addField("edges", Qubit.product(Qubit));
+		Field location = circGraph.addField("location", Qubit.product(Machine));
+		Field numTele = circGraph.addField("numTele", Sig.SIGINT);
 
-            // Create concrete Machine sigs
-            PrimSig M_0 = new PrimSig("M_0", Machine, Attr.ONE);
-            PrimSig M_1 = new PrimSig("M_1", Machine, Attr.ONE);
-            PrimSig M_2 = new PrimSig("M_2", Machine, Attr.ONE);
+		Decl q = Qubit.oneOf("q");
 
-            // Create the circGraph sig
-            PrimSig circGraph = new PrimSig("circGraph");
+		// all q:Q | #q.location = 1
+		Expr qloc = q.get().join(location.decl().get())
+			.cardinality()
+			.eq(ExprConstant.makeNUMBER(1))
+			.forall(q);
 
-            // Fields for circGraph
-            Field edges = circGraph.addField("edges", Qubit.product(Qubit));
-            Field location = circGraph.addField("location", Qubit.product(Machine));
-            Field numTele = circGraph.addField("numTele", Sig.Int);
+		circGraph.addFact(qloc);
 
-            // Add a constraint that all Qubits have exactly one location
-            Expr factExpr1 = ExprUnary.Op.NOOP.make(Pos.UNKNOWN, Qubit).apply(Pos.UNKNOWN, ExprUnary.Op.CARDINALITY.make(Pos.UNKNOWN, location));
-            Expr factExpr2 = ExprUnary.Op.NOOP.make(Pos.UNKNOWN, factExpr1).apply(Pos.UNKNOWN, ExprBinary.Op.EQUALS.make(Pos.UNKNOWN, factExpr1, ExprConstant.Op.CONST1.make(Pos.UNKNOWN)));
+		Decl c = circGraph.oneOf("c");
+		Decl m = Machine.oneOf("m");
+		
+		// for all c:cG, m:M | #(c.location).m < CAPACITY
+		Expr ccloc = c.get().join(location.decl().get())
+			.join(m.get())
+			.cardinality()
+			.lt(ExprConstant.makeNUMBER(CAPACITY))
+			.forAll(c, m);
 
-            Expr fact1 = ExprUnary.Op.ALL.make(Pos.UNKNOWN, factExpr2);
+		// add ccloc as a fact
+		circGraph.addFact(ccloc);
 
-            world.addFact("fact1", fact1);
-
-            // Fact: all circGraph, all Machine | #(circGraph.location).Machine < 3
-            Expr factExpr3 = ExprBinary.Op.LT.make(Pos.UNKNOWN, ExprUnary.Op.CARDINALITY.make(Pos.UNKNOWN, location), ExprConstant.Op.CONST3.make(Pos.UNKNOWN));
-            Expr factExpr4 = ExprUnary.Op.ALL.make(Pos.UNKNOWN, factExpr3);
-
-            world.addFact("fact2", factExpr4);
-
-            // The CircuitGraph fact
-            Expr firstGraph = ExprUnary.Op.NOOP.make(Pos.UNKNOWN, circGraph).apply(Pos.UNKNOWN, ExprUnary.Op.CARDINALITY.make(Pos.UNKNOWN, circGraph));
-
-            // Complex fact construction with lets, corresponding to the CircuitGraph fact in Alloy
-            Expr letL0 = ExprUnary.Op.NOOP.make(Pos.UNKNOWN, firstGraph).apply(Pos.UNKNOWN, ExprBinary.Op.EQUALS.make(Pos.UNKNOWN, firstGraph, ExprConstant.Op.CONST0.make(Pos.UNKNOWN)));
-
-            // Construct the remaining parts of the CircuitGraph fact
-            // Each of these corresponds to steps in the Alloy model's let statements
-
-            // For simplicity, we won't build all intermediate steps in Java, but you would
-            // need to continue constructing each subsequent 'let' statement similarly.
-
-            // A complete implementation would look similar for each step from l_0 to l_10
-
-            // Adding the final fact (CircuitGraph) to the model
-            world.addFact("CircuitGraph", letL0);
-
-            // The teleport predicate
-            Decl declLoc = location.oneOf(Pos.UNKNOWN, "loc");
-            Decl declR = edges.oneOf(Pos.UNKNOWN, "r");
-            Decl declULoc = location.oneOf(Pos.UNKNOWN, "uloc");
-            Decl declTele = numTele.oneOf(Pos.UNKNOWN, "tele");
-            Decl declUTele = numTele.oneOf(Pos.UNKNOWN, "utele");
-
-            // Constructing the body of the teleport predicate
-            Expr predExpr1 = ExprBinary.Op.IMPLIES.make(Pos.UNKNOWN, declR.get().one(Pos.UNKNOWN), declULoc.get().one(Pos.UNKNOWN));
-            Expr predExpr2 = ExprBinary.Op.EQUALS.make(Pos.UNKNOWN, declUTele.get().one(Pos.UNKNOWN), ExprBinary.Op.PLUS.make(Pos.UNKNOWN, declTele.get().one(Pos.UNKNOWN), ExprUnary.Op.CARDINALITY.make(Pos.UNKNOWN, declULoc.get().one(Pos.UNKNOWN))));
-
-            // Combine into the teleport predicate
-            Expr teleportBody = ExprUnary.Op.ALL.make(Pos.UNKNOWN, ExprBinary.Op.AND.make(Pos.UNKNOWN, predExpr1, predExpr2));
-
-            world.addPredicate("teleport", teleportBody, declLoc, declR, declULoc, declTele, declUTele);
-
-            // Fact: layerTransition
-            Expr factLayerTransition = ExprUnary.Op.ALL.make(Pos.UNKNOWN, teleportBody);
-
-            world.addFact("layerTransition", factLayerTransition);
-
-            // The finalLayer predicate
-            Expr finalLayerExpr = ExprBinary.Op.LTE.make(Pos.UNKNOWN, ExprUnary.Op.NOOP.make(Pos.UNKNOWN, numTele), ExprConstant.Op.CONST6.make(Pos.UNKNOWN));
-            world.addPredicate("finalLayer", finalLayerExpr);
-
-            // Set up the run command
-            Command runCommand = new Command(Pos.UNKNOWN, "run", false, 11, 7, finalLayerExpr);
-
-            // Add command to the world
-            world.addCommand(runCommand);
-
-            // Solve using the Alloy API
-            A4Options options = new A4Options();
-            options.solver = A4Options.SatSolver.SAT4J;
-
-            A4Solution solution = TranslateAlloyToKodkod.execute_command(null, world.getAllReachableSigs(), runCommand, options);
-
-            // Output the result
-            if (solution.satisfiable()) {
-                System.out.println("Solution found:\n" + solution.toString());
-            } else {
-                System.out.println("No solution found.");
-            }
-            */
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+		/*
+		// Add a constraint that all Qubits have exactly one location
+		Expr factExpr1 = ExprUnary.Op.NOOP.make(Pos.UNKNOWN, Qubit).apply(Pos.UNKNOWN, ExprUnary.Op.CARDINALITY.make(Pos.UNKNOWN, location));
+		Expr factExpr2 = ExprUnary.Op.NOOP.make(Pos.UNKNOWN, factExpr1).apply(Pos.UNKNOWN, ExprBinary.Op.EQUALS.make(Pos.UNKNOWN, factExpr1, ExprConstant.Op.CONST1.make(Pos.UNKNOWN)));
+		
+		Expr fact1 = ExprUnary.Op.ALL.make(Pos.UNKNOWN, factExpr2);
+		
+		world.addFact("fact1", fact1);
+		
+		// Fact: all circGraph, all Machine | #(circGraph.location).Machine < 3
+		Expr factExpr3 = ExprBinary.Op.LT.make(Pos.UNKNOWN, ExprUnary.Op.CARDINALITY.make(Pos.UNKNOWN, location), ExprConstant.Op.CONST3.make(Pos.UNKNOWN));
+		Expr factExpr4 = ExprUnary.Op.ALL.make(Pos.UNKNOWN, factExpr3);
+		
+		world.addFact("fact2", factExpr4);
+		
+		// The CircuitGraph fact
+		Expr firstGraph = ExprUnary.Op.NOOP.make(Pos.UNKNOWN, circGraph).apply(Pos.UNKNOWN, ExprUnary.Op.CARDINALITY.make(Pos.UNKNOWN, circGraph));
+		
+		// Complex fact construction with lets, corresponding to the CircuitGraph fact in Alloy
+		Expr letL0 = ExprUnary.Op.NOOP.make(Pos.UNKNOWN, firstGraph).apply(Pos.UNKNOWN, ExprBinary.Op.EQUALS.make(Pos.UNKNOWN, firstGraph, ExprConstant.Op.CONST0.make(Pos.UNKNOWN)));
+		
+		// Construct the remaining parts of the CircuitGraph fact
+		// Each of these corresponds to steps in the Alloy model's let statements
+		
+		// For simplicity, we won't build all intermediate steps in Java, but you would
+		// need to continue constructing each subsequent 'let' statement similarly.
+		
+		// A complete implementation would look similar for each step from l_0 to l_10
+		
+		// Adding the final fact (CircuitGraph) to the model
+		world.addFact("CircuitGraph", letL0);
+		
+		// The teleport predicate
+		Decl declLoc = location.oneOf(Pos.UNKNOWN, "loc");
+		Decl declR = edges.oneOf(Pos.UNKNOWN, "r");
+		Decl declULoc = location.oneOf(Pos.UNKNOWN, "uloc");
+		Decl declTele = numTele.oneOf(Pos.UNKNOWN, "tele");
+		Decl declUTele = numTele.oneOf(Pos.UNKNOWN, "utele");
+		
+		// Constructing the body of the teleport predicate
+		Expr predExpr1 = ExprBinary.Op.IMPLIES.make(Pos.UNKNOWN, declR.get().one(Pos.UNKNOWN), declULoc.get().one(Pos.UNKNOWN));
+		Expr predExpr2 = ExprBinary.Op.EQUALS.make(Pos.UNKNOWN, declUTele.get().one(Pos.UNKNOWN), ExprBinary.Op.PLUS.make(Pos.UNKNOWN, declTele.get().one(Pos.UNKNOWN), ExprUnary.Op.CARDINALITY.make(Pos.UNKNOWN, declULoc.get().one(Pos.UNKNOWN))));
+		
+		// Combine into the teleport predicate
+		Expr teleportBody = ExprUnary.Op.ALL.make(Pos.UNKNOWN, ExprBinary.Op.AND.make(Pos.UNKNOWN, predExpr1, predExpr2));
+		
+		world.addPredicate("teleport", teleportBody, declLoc, declR, declULoc, declTele, declUTele);
+		
+		// Fact: layerTransition
+		Expr factLayerTransition = ExprUnary.Op.ALL.make(Pos.UNKNOWN, teleportBody);
+		
+		world.addFact("layerTransition", factLayerTransition);
+		
+		// The finalLayer predicate
+		Expr finalLayerExpr = ExprBinary.Op.LTE.make(Pos.UNKNOWN, ExprUnary.Op.NOOP.make(Pos.UNKNOWN, numTele), ExprConstant.Op.CONST6.make(Pos.UNKNOWN));
+		world.addPredicate("finalLayer", finalLayerExpr);
+		
+		// Set up the run command
+		Command runCommand = new Command(Pos.UNKNOWN, "run", false, 11, 7, finalLayerExpr);
+		
+		// Add command to the world
+		world.addCommand(runCommand);
+		
+		// Solve using the Alloy API
+		A4Options options = new A4Options();
+		options.solver = A4Options.SatSolver.SAT4J;
+		
+		A4Solution solution = TranslateAlloyToKodkod.execute_command(null, world.getAllReachableSigs(), runCommand, options);
+		
+		// Output the result
+		if (solution.satisfiable()) {
+		    System.out.println("Solution found:\n" + solution.toString());
+		} else {
+		    System.out.println("No solution found.");
+		}
+		*/
+	}
 }
